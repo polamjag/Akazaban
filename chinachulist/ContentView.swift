13a955efb665fac9a83dee3a9d039bc7848a3fa1
polamjag <s@polamjag.info>
2020-03-01 23:00:24 +0900

    aaa

--- chinachulist/ContentView.swift
+++ chinachulist/ContentView.swift
@@ -33,64 +33,13 @@ struct ContentView: View {
                     action: { self.showModal.toggle() },
                     label: { Image(systemName: "gear") }
                 ).sheet(isPresented: $showModal) {
-                    ModalView(showModal: self.$showModal)
+                    SettingsView(showModal: self.$showModal)
                 }
             )
             .navigationBarTitle("chinachu")
         }
     }
 
-}
-
-struct ModalView: View {
-    @Binding var showModal: Bool
-    
-    var body: some View {
-        NavigationView {
-            List {
-                Section(header: Text("Logs")) {
-                    NavigationLink(destination: ChinachuLogView(logType: .logTypeWui)) {
-                        VStack {
-                            Text("wui")
-                        }
-                    }
-                    NavigationLink(destination: ChinachuLogView(logType: .logTypeOperator)) {
-                        VStack {
-                            Text("operator")
-                        }
-                    }
-                    NavigationLink(destination: ChinachuLogView(logType: .logTypeScheduler)) {
-                        VStack {
-                            Text("scheduler")
-                        }
-                    }
-                }
-            }
-            .navigationBarTitle("Settings")
-        }
-    }
-}
-
-struct ChinachuLogView: View {
-    @ObservedObject var logFetcher = LogFetcher()
-    var logType: ChinachuLogType
-    
-    var body: some View {
-        ScrollView {
-            Text(logFetcher.log)
-                .font(.custom("Menlo", size: 12.0))
-                .padding()
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-        }.onAppear {
-            self.logFetcher.get(logType: self.logType)
-        }
-    }
-}
-
-struct ModalView_Previews: PreviewProvider {
-    static var previews: some View {
-        ModalView(showModal: .constant(true))
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ chinachulist/SettingsView.swift
@@ -0,0 +1,60 @@
+//
+//  SettingsView.swift
+//  chinachulist
+//
+//  Created by Satoru ABE on 2020/03/01.
+//  Copyright © 2020 Satoru ABE. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @Binding var showModal: Bool
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Logs")) {
+                    NavigationLink(destination: ChinachuLogView(logType: .logTypeWui)) {
+                        VStack {
+                            Text("wui")
+                        }
+                    }
+                    NavigationLink(destination: ChinachuLogView(logType: .logTypeOperator)) {
+                        VStack {
+                            Text("operator")
+                        }
+                    }
+                    NavigationLink(destination: ChinachuLogView(logType: .logTypeScheduler)) {
+                        VStack {
+                            Text("scheduler")
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Settings")
+        }
+    }
+}
+
+struct ChinachuLogView: View {
+    @ObservedObject var logFetcher = LogFetcher()
+    var logType: ChinachuLogType
+    
+    var body: some View {
+        ScrollView {
+            Text(logFetcher.log)
+                .font(.custom("Menlo", size: 12.0))
+                .padding()
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+        }.onAppear {
+            self.logFetcher.get(logType: self.logType)
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView(showModal: .constant(true))
+    }
+}
